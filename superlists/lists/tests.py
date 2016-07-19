f68b5b60633f1db3a5b86115bcaec8217b5dfa9f
Richard Huynh <huynhtastic@gmail.com>
2016-07-19 15:14:45 -0500

    Model for list Items and associated migration; fixed returnsCorrectHTML test

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -3,6 +3,7 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.http import HttpRequest
 
+from lists.models import Item
 from lists.views import home_page
 
 # Create your tests here.
@@ -22,16 +23,9 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-
-		self.assertIn('A new list item', response.content.decode())
-
-		# home.html converted to string
-		# now adding context variable to pass to home.html to render then convert
-
-		expected_html = render_to_string(
-			'home.html',
-			{'new_item_text': 'A new list item'}
-		)
+		expected_html = render_to_string('home.html', request=request)
+		print(response.content.decode())
+		print(expected_html)
 		self.assertEqual(response.content.decode(), expected_html)
 
 	def test_home_page_can_save_a_POST_request(self):
@@ -42,3 +36,22 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 
 		self.assertIn('A new list item', response.content.decode())
+
+class ItemModelTest(TestCase):
+
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')
